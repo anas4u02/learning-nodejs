47116414be367eaca9837441a6c2f68e3f142292
Mohammad Anas <anas4u02@gmail.com>
2023-11-28 01:20:39 +0530

    Writing into file

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -3,3 +3,6 @@ const fs = require('fs');
 
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 log(textIn);
+
+const textOut = `This is what we know about Avocado ${textIn}. Created on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
