cd853342162a10e5617ba420002c74e419cc5aae
Mohammad Anas <anas4u02@gmail.com>
2023-11-28 01:47:52 +0530

    Implemented asynchrnous methods for file handling

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,19 @@
 const { log } = require('console');
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-log(textIn);
+fs.readFile('./txt/start.txt', 'utf8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf8', (err, data2) => {
+        fs.readFile('./txt/append.txt', 'utf8', (err, data3) => {
+            log(data3);
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}` ,'utf8', err => {
+                log("Your file has been written");
+            });
+        });
+        log(data2);
+    });
+    log(data1);
+});
+log("Reading asynchrnously");
 
-const textOut = `This is what we know about Avocado ${textIn}. Created on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
+// const textOut = `This is what we know about Avocado ${textIn}. Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
