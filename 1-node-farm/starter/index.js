a466621fc3f2e78949e82fbee681107b81c5f1cf
Mohammad Anas <anas4u02@gmail.com>
2023-11-28 23:50:44 +0530

    Created a server with a basic req, response

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,9 @@
 const { log } = require('console');
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-fs.readFile('./txt/start.txt', 'utf8', (err, data1) => {
+/**fs.readFile('./txt/start.txt', 'utf8', (err, data1) => {
     fs.readFile(`./txt/${data1}.txt`, 'utf8', (err, data2) => {
         fs.readFile('./txt/append.txt', 'utf8', (err, data3) => {
             log(data3);
@@ -17,3 +19,22 @@ log("Reading asynchrnously");
 
 // const textOut = `This is what we know about Avocado ${textIn}. Created on ${Date.now()}`;
 // fs.writeFileSync('./txt/output.txt', textOut);
+ */
+
+// ///////////////SERVER/////////////////////
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end("This is the overview");
+    } else if (pathName === '/product') {
+        res.end("This is product");
+    } else {
+        res.writeHead(404);
+        res.end("Page Not Found!!")
+    }
+});
+
+server.listen(8000, () => {
+    log("Server started on port 8000");
+});
